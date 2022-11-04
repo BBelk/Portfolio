c7b8707cb950db6f455ac6d67a61267145b5be9e
Belkbh <belkbh@gmail.com>
2022-11-04 14:08:50 -0400

    updated contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,28 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
-export default function Contact() {
+function Contact() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log('Submit Form', formState);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log('Handle Form', formState);
+    }
+  };
+
   return (
-    <div className='container'>
-      <h1>Contact</h1>
-      <form>
-        <div className="mb-2">
-          <label for="exampleFormControlInput1" className="form-label">
-            Name
-          </label>
-          <input type="email" className="form-control" id="exampleFormControlInput1" />
-          <label for="exampleFormControlInput1" className="form-label">
-            Email Address
-          </label>
-          <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
-        </div>
-        <div className="mb-3">
-          <label for="exampleFormControlTextarea1" className="form-label" >Message</label>
-          <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-        </div>
-        <div class="col-auto">
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-      </form>
+    <div className='d-flex justify-content-center'>
+
+      <section className='contact-form'>
+        <h1 className='heroHeader'>Contact Me</h1>
+        <form id="form-handler" onSubmit={handleSubmit}>
+          
+            <p className='contact-headers'>Name:</p>
+            <input className="contact-input" type="text" name="name" defaultValue={name} onBlur={handleChange} />
+
+            <p className='contact-headers'>Email:</p>
+            <input className="contact-input" type="email" name="email" defaultValue={email} onBlur={handleChange} />
+
+            <p className='contact-headers'>Message:</p>
+            <textarea className="contact-input" name="message" defaultValue={message} onBlur={handleChange} />
+
+            <div className="button">
+              <button data-testid="button" type="submit">Submit</button>
+            </div>
+         
+        </form>
+      </section>
     </div>
   );
 }
+
+export default Contact;
