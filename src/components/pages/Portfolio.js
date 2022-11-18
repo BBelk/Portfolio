5c9a02b9b68f51386e1534067d1a05a0a25f5c5f
Belkbh <belkbh@gmail.com>
2022-11-17 22:36:36 -0500

    Removed old portfolio page code and deleted old images

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -114,56 +114,5 @@ export default function Blog() {
     </div>
       
   </div>
-//     <div className='p-3'>
-//       <h1>Portfolio</h1>
-// {/* <Gallery images={images} /> */}
-//       <div style={{display:'flex',
-//       'flex-wrap':'wrap',
-//       // display:'grid',
-//     gridTemplateRows: 'auto',
-//     rowGap: '5ch',
-//     gridTemplateColumns: '1fr 1fr',
-//     padding: '50px'
-//     }}>
-//     <div className="img-thumbnail" style= {style.imgStyle}>
-//     <img src={vanGame} alt='profile pic' style={{height:'100%', width:'100%'}} />
-//     <h3>The Van Game 
-//     <a href="https://store.steampowered.com/app/2081860/The_Van_Game/">
-//       <img  src={gitHubPic} alt='github logo' style= {style.logoStyle}/>
-//     </a>
-//       </h3> 
-//     </div>
-
-//     <div className="img-thumbnail" style= {style.imgStyle}>
-//     <img src={flower1} alt='profile pic' style={{height:'100%', width:'100%'}} />
-//     <h3>NotInsta-Clone 
-//     <a href="https://www.github.com/Test">
-//       <img  src={gitHubPic} alt='github logo' style= {style.logoStyle}/>
-//     </a>
-//       </h3> 
-//     </div>
-
-//     <div className="img-thumbnail" style= {style.imgStyle}>
-//     <img src={flower2} alt='profile pic' style={{height:'100%', width:'100%'}} />
-//     <h3>Pokemon Giphy
-//     <a href="https://www.github.com/Test">
-//       <img  src={gitHubPic} alt='github logo' style= {style.logoStyle}/>
-//     </a>
-//       </h3> 
-//     </div>
-
-//     <div className="img-thumbnail" style= {style.imgStyle}>
-//     <img src={flower3} alt='profile pic' style={{height:'100%', width:'100%'}} />
-//     <h3>A third Thing
-//     <a href="https://www.github.com/Test">
-//       <img  src={gitHubPic} alt='github logo' style= {style.logoStyle}/>
-//     </a>
-//       </h3> 
-//     </div>
-
-      
-//       </div>
-        
-//     </div>
   );
 }
